d872247f3812db99481e9b2880c356a8b8eb84b5
Eshwar Seerdat <eshwarseerdat@gmail.com>
2022-10-19 07:30:23 -0400

    create field class

---
+++ main.js
@@ -0,0 +1,30 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
+const moveDirection = "wsad";
+
+class Field {
+  constructor(field) {
+    this._field = field;
+  }
+
+  get field() {
+    return this._field;
+  }
+
+  set field(newField) {
+    this._field = newField;
+  }
+
+  print() {
+    const fieldString = this._field.reduce((str, current) => {
+      str += current.join("") + "\n";
+      return str;
+    }, "");
+
+    console.log(fieldString);
+  }
+}
